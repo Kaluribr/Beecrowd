a48ec41cf3c839cf620b8c071810d8a511f73cf8
Danilo Moraes <danilot.moraes@gmail.com>
2023-02-28 19:34:02 -0300

    exercicios do beecrownd

---
+++ src/beecrowd/VetorSelecao.java
@@ -0,0 +1,28 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class VetorSelecao {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+
+		Double[] A = new Double[10];
+		double x;
+
+		for (int i = 0; i < A.length; i++) {
+			x = ler.nextDouble();
+			A[i] = x;
+		}
+		// A[i] = 0.0
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] <= 10) {
+				System.out.println("A [" + i + "] = " + A[i]);
+			}
+		}
+
+	}
+
+
+}
